4d7f2510f78a8431fb87c218b69dbf71ec937609
Mildred Ochieng <48028046+AdahMilly@users.noreply.github.com>
2020-09-26 17:10:27 +0300

    Update users.service.js

--- src/domains/users/users.service.js
+++ src/domains/users/users.service.js
@@ -1,28 +1,40 @@
-import EncryptData from '../../lib/helpers/encrypt';
-import createToken from '../../lib/helpers/jwtHelper';
-import CustomError from '../../lib/utils/customError';
-import { getConfig } from '../../config';
-import { usersResource } from './users.resource';
+import EncryptData from "../../lib/helpers/encrypt";
+import createToken from "../../lib/helpers/jwtHelper";
+import CustomError from "../../lib/utils/customError";
+import { getConfig } from "../../config";
+import { usersResource } from "./users.resource";
+import { sendSms } from "../../lib/utils/smsSender";
 
 const config = getConfig();
 
 class UsersService {
   async create(createUserBody) {
     const { email, password, name, phoneNumber } = createUserBody;
-    console.log(createUserBody);
-    const existingUser = await usersResource.getUser('email', email);
+
+    const existingUser = await usersResource.getUser("email", email);
     if (existingUser) {
-      throw new CustomError(409, 'User already exists');
+      throw new CustomError(409, "User already exists");
     }
     const encryptedPassword = EncryptData.generateHash(password);
     const createdUser = await usersResource.create({
       email,
       name,
       password: encryptedPassword,
-      phone: phoneNumber
+      phone: phoneNumber,
     });
 
-    const token = createToken({ id: createdUser.id,name: createdUser.name }, config.secretKey);
+    let contact;
+    if (phoneNumber.startsWith(0)) {
+      contact = "+254" + phoneNumber.substr(1);
+    } else {
+      contact = phoneNumber;
+    }
+
+    await sendSms({ to: contact, message: `Hello ${name}, welcome to fancy events` });
+    const token = createToken(
+      { id: createdUser.id, name: createdUser.name },
+      config.secretKey
+    );
 
     const registeredUser = {
       ...createdUser,
@@ -38,7 +50,7 @@ class UsersService {
     return token;
   }
 
-  async getMyRsvps (userId) {
+  async getMyRsvps(userId) {
     const events = await usersResource.getMyRsvps(userId);
     return events;
   }

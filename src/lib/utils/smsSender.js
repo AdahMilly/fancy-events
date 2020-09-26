66d684bb5eac5be5e835ba9a546ec2ee191c6ec4
Mildred Ochieng <48028046+AdahMilly@users.noreply.github.com>
2020-09-26 16:33:31 +0300

    Update smsSender.js

--- src/lib/utils/smsSender.js
+++ src/lib/utils/smsSender.js
@@ -7,8 +7,7 @@ const smsService =  AfricasTalking({ apiKey, username}).SMS;
 
 export const sendSms = async (smsDetails) => {
   try {
-    const response = await smsService.send(smsDetails);
-    console.log('sms sent successfuly', response)
+    await smsService.send(smsDetails);
   } catch(error){
     console.log('error while sending sms', error)
   }

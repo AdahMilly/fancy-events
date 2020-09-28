e6da23ad452549193d04600ab33a94bdc5294a75
Mildred Ochieng <48028046+AdahMilly@users.noreply.github.com>
2020-09-28 12:51:49 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import errorHandler from './lib/middlewares/globalErrorHandler';
 import { getUsersRouter } from './domains/users/users.routes';
 import { getEventsRouter } from './domains/events/events.routes';
@@ -15,6 +16,7 @@ class App {
 
   createExpressApp() {
     const app = express();
+    app.use(cors());
     app.use(express.json());
     app.get(
       '/',

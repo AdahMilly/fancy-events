d29bd2db572267110d5a11ac8ff158578a1cd750
Mildred Ochieng <48028046+AdahMilly@users.noreply.github.com>
2020-10-05 18:39:09 +0300

    Update events.routes.js

--- src/domains/events/events.routes.js
+++ src/domains/events/events.routes.js
@@ -1,91 +1,101 @@
-import { Router } from 'express';
-import { eventsService } from './events.service';
-import { protectedAsyncRequestHandler } from '../../lib/utils/protectedAsyncHandler';
-import { jwtAuthentication } from '../../lib/utils/passportSetup';
-import { eventResource } from './events.resource';
+import { Router } from "express";
+import { eventsService } from "./events.service";
+import { protectedAsyncRequestHandler } from "../../lib/utils/protectedAsyncHandler";
+import { jwtAuthentication } from "../../lib/utils/passportSetup";
 
-export function getEventsRouter(){
+export function getEventsRouter() {
   const eventsRouter = Router();
 
   eventsRouter.post(
-    '/events',
+    "/events",
     jwtAuthentication,
-    protectedAsyncRequestHandler( async (req, res) => {
-      const event = await eventsService.create({...req.body, createdBy: req.user.id});
-      res.status(201).json({ message: 'event created', event});
+    protectedAsyncRequestHandler(async (req, res) => {
+      const event = await eventsService.create({
+        ...req.body,
+        createdBy: req.user.id,
+      });
+      res.status(201).json({ message: "event created", event });
     })
-    )
-   
-  eventsRouter.get(
-    '/events',
-    protectedAsyncRequestHandler( async (req, res) => {
-      const events = await eventsService.get();
-      res.status(200).json({ message: 'events', events});
-    })
-  )
+  );
 
   eventsRouter.get(
-    '/events/:id',
-    protectedAsyncRequestHandler( async (req, res) => {
-      const event = await eventsService.getEvent('id', req.params.id);
-      res.status(200).json({ message: 'success', event});
+    "/events",
+    protectedAsyncRequestHandler(async (req, res) => {
+      const events = await eventsService.get();
+      res.status(200).json({ message: "events", events });
     })
-  )
+  );
+
+  eventsRouter.get(
+    "/events/:id",
+    protectedAsyncRequestHandler(async (req, res) => {
+      const event = await eventsService.getEvent("id", req.params.id);
+      res.status(200).json({ message: "success", event });
+    })
+  );
+
+  eventsRouter.get(
+    "/my-events",
+    jwtAuthentication,
+    protectedAsyncRequestHandler(async (req, res) => {
+      const events = await eventsService.getMyEvents(req.user.id);
+      res.status(200).json({ message: "success", events });
+    })
+  );
 
   eventsRouter.put(
-    '/events/:id',
+    "/events/:id",
     jwtAuthentication,
-    protectedAsyncRequestHandler( async (req, res) => {
+    protectedAsyncRequestHandler(async (req, res) => {
       const event = await eventsService.updateEvent(req.body, req.params.id);
-      res.status(200).json({ message: 'success', event});
+      res.status(200).json({ message: "success", event });
     })
-
-  )
+  );
 
   eventsRouter.delete(
-    '/events/:id',
+    "/events/:id",
     jwtAuthentication,
-    protectedAsyncRequestHandler( async (req, res) => {
+    protectedAsyncRequestHandler(async (req, res) => {
       await eventsService.deleteEvent(req.params.id);
-      res.status(200).json({message: 'event deleted successfuly'});
+      res.status(200).json({ message: "event deleted successfuly" });
     })
-  )
+  );
 
   eventsRouter.post(
-    '/events/:id/rsvp',
+    "/events/:id/rsvp",
     jwtAuthentication,
-    protectedAsyncRequestHandler( async (req, res) => {
-      await eventsService.rsvp(req.params.id, req.user.id)
-      res.status(201).json({ message: 'ticket reserved successfuly'})
+    protectedAsyncRequestHandler(async (req, res) => {
+      await eventsService.rsvp(req.params.id, req.user.id);
+      res.status(201).json({ message: "ticket reserved successfuly" });
     })
-  )
+  );
 
   eventsRouter.get(
-    '/events/:id/rsvps',
+    "/events/:id/rsvps",
     jwtAuthentication,
-    protectedAsyncRequestHandler( async (req, res) => {
+    protectedAsyncRequestHandler(async (req, res) => {
       const guests = await eventsService.getRsvps(req.params.id);
-      res.status(200).json({ message: 'success', guests});
+      res.status(200).json({ message: "success", guests });
     })
-  )
+  );
 
   eventsRouter.put(
-    '/events/:id/rsvps/cancel',
+    "/events/:id/rsvps/cancel",
     jwtAuthentication,
-    protectedAsyncRequestHandler( async (req, res) => {
+    protectedAsyncRequestHandler(async (req, res) => {
       await eventsService.cancelRsvp(req.params.id, req.user.id);
-      res.status(200).json({ message: 'rsvp cancelled successfuly'});
+      res.status(200).json({ message: "rsvp cancelled successfuly" });
     })
-  )
+  );
 
   eventsRouter.put(
-    'events/:id/cancel',
+    "/events/:id/cancel",
     jwtAuthentication,
     protectedAsyncRequestHandler(async (req, res) => {
       await eventsService.cancelEvent(req.params.id);
-      res.status(200).json({ message: 'event cancelled successfuly'});
+      res.status(200).json({ message: "event cancelled successfuly" });
     })
-  )
-  
+  );
+
   return eventsRouter;
 }

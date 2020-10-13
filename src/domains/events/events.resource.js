78a751171d30c6f483cb4ed8c2d68b2a0a1c2ea7
Mildred Ochieng <48028046+AdahMilly@users.noreply.github.com>
2020-10-13 11:03:32 +0300

    Update events.resource.js
    return event guests when fetching an event

--- src/domains/events/events.resource.js
+++ src/domains/events/events.resource.js
@@ -1,73 +1,79 @@
-import { knexInstance } from '../../database/knexInstance';
+import { knexInstance } from "../../database/knexInstance";
 
-const EVENTS_TABLE_NAME = 'events';
-const RSVPS_TABLE = 'rsvps';
-const USERS_TABLE = 'users';
+const EVENTS_TABLE_NAME = "events";
+const RSVPS_TABLE = "rsvps";
+const USERS_TABLE = "users";
 
-class EventsResource{
-  async create(eventBody){
-    const created = await knexInstance(EVENTS_TABLE_NAME).insert(eventBody, '*');
+class EventsResource {
+  async create(eventBody) {
+    const created = await knexInstance(EVENTS_TABLE_NAME).insert(
+      eventBody,
+      "*"
+    );
     return created[0];
   }
-  async get(){
-    const events = await knexInstance(EVENTS_TABLE_NAME).select('*')
+  async get() {
+    const events = await knexInstance
+      .select("event.*", 'rsvp.event_id', knexInstance.raw('ARRAY_AGG(rsvp.user_id) as guests'))
+      .from({ event: EVENTS_TABLE_NAME })
+      .leftJoin({ rsvp: RSVPS_TABLE }, 'rsvp.event_id', 'event.id' )
+      .where('event.cancelled', false)
+      .groupBy('event.id', 'rsvp.event_id')
     return events;
   }
 
-  async getEvent(feild, value){
-  const event = await knexInstance(EVENTS_TABLE_NAME)
-    .select('*')
-    .where(feild, value)
-    .first()
+  async getEvent(feild, value) {
+    const event = await knexInstance(EVENTS_TABLE_NAME)
+      .select("*")
+      .where(feild, value)
+      .first();
 
     return event;
   }
 
-  async getMyEvents(userId){
-    const events = await knexInstance(EVENTS_TABLE_NAME).select('*').where('created_by', userId)
+  async getMyEvents(userId) {
+    const events = await knexInstance(EVENTS_TABLE_NAME)
+      .select("*")
+      .where("created_by", userId);
     return events;
   }
 
-  async updateEvent(updateEventBody, eventId){
+  async updateEvent(updateEventBody, eventId) {
     const updatedEvent = await knexInstance(EVENTS_TABLE_NAME)
-    .update(updateEventBody, '*')
-    .where('id',eventId)
+      .update(updateEventBody, "*")
+      .where("id", eventId);
 
     return updatedEvent;
   }
 
-  async deleteEvent(eventId){
-    return knexInstance(EVENTS_TABLE_NAME)
-      .delete()
-      .where('id', eventId)
+  async deleteEvent(eventId) {
+    return knexInstance(EVENTS_TABLE_NAME).delete().where("id", eventId);
   }
 
-  async rsvp(eventId, userId){
-    return knexInstance(RSVPS_TABLE)
-      .insert({eventId, userId});
+  async rsvp(eventId, userId) {
+    return knexInstance(RSVPS_TABLE).insert({ eventId, userId });
   }
 
-  async getRsvps(eventId){
+  async getRsvps(eventId) {
     const guests = await knexInstance(RSVPS_TABLE)
-    .select(['name', 'phone'])
-    .innerJoin(USERS_TABLE, `${USERS_TABLE}.id`, `${RSVPS_TABLE}.user_id`)
-    .where(`${RSVPS_TABLE}.event_id`, eventId)
-    .where(`${RSVPS_TABLE}.cancelled`, false);
-    return guests; 
+      .select(["name", "phone", "id"])
+      .innerJoin(USERS_TABLE, `${USERS_TABLE}.id`, `${RSVPS_TABLE}.user_id`)
+      .where(`${RSVPS_TABLE}.event_id`, eventId)
+      .where(`${RSVPS_TABLE}.cancelled`, false);
+    return guests;
   }
 
   async cancelRsvp(eventId, userId) {
     return knexInstance(RSVPS_TABLE)
-      .update({cancelled: true})
-      .where({ eventId, userId })
+      .update({ cancelled: true })
+      .where({ eventId, userId });
   }
 
-  async cancelEvent(eventId){
+  async cancelEvent(eventId) {
     return knexInstance(EVENTS_TABLE_NAME)
-    .update({cancelled:true})
-    .where('id', eventId)
+      .update({ cancelled: true })
+      .where("id", eventId);
   }
-
 }
 
 export const eventResource = new EventsResource();
